76236181f72553db70f27b89a4d5d88ed707eaf2
alehosainim-afk <alehosaini.m@gmail.com>
2026-06-15 21:37:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ async function connectDB() {
 }
  
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log('Headers:', JSON.stringify(req.headers));
+  next();
+});
 app.use(express.static('.'));
 app.use(session({
   secret: process.env.SESSION_SECRET,
